c8a22e3eb352647644f405934609d1476d1ea4db
Shubham <51260891+Nova-Spectre@users.noreply.github.com>
2024-04-04 14:59:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,13 +31,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors({
-//     origin:["https://social-circle-frontend-snowy.vercel.app"],
-//     methods:["POST","GET","PATCH"],
-//     credential:true
-// }
-// ));
-app.use(cors());
+app.use(cors({
+    origin:["https://social-circle-frontend-snowy.vercel.app"],
+    methods:["POST","GET","PATCH"],
+    credential:true
+}
+));
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* File Storage */
